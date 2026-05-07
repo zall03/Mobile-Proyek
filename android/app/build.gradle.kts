0554514306d7467bd93022098e12f987a0252f3e
anestyahmadfauziah <anstyahmdfzh@gmail.com>
2026-05-07 15:26:01 +0700

    update app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 android {
     namespace = "com.example.project2"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = ""28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
